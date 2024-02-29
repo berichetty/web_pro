14cff446253c05b804d2285b7f5db490caa23383
berichetty <153484160+berichetty@users.noreply.github.com>
2024-02-29 16:25:59 +0530

    port
    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");//es5 // importing json-server library
+
+const server = jsonServer.create(); // created 
+const router = jsonServer.router("ganesh.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router); 
+server.listen(port);
